e682890e12df7407b8344d6b5ceb4c72f7c41a1b
jeeva <rishivickey7@gmail.com>
2021-04-09 14:39:44 +0530

    end of session 13

---
+++ Core/Interface/IBasketRepository.cs
@@ -0,0 +1,12 @@
+﻿using System.Threading.Tasks;
+using Core.Entity;
+
+namespace Core.Interface
+{
+    public interface IBasketRepository
+    {
+        Task<CustomerBasket> GetBasketAsync(string basketId);
+        Task<CustomerBasket> UpdateBasketAsync(CustomerBasket basket);
+        Task<bool> DeleteBasketAsync(string basketId);
+    }
+}
